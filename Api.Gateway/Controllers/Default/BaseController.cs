0ed021fa28b726f4ccc5774e078f7b958a0ae85c
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2020-09-08 15:59:31 -0300

    1 - imlementing api gateway

---
+++ Api.Gateway/Controllers/Default/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Messages.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.Gateway.Controllers.Default
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected async Task<IActionResult> WithResponseAsync<TResponseMessage>(Func<Task<Response<TResponseMessage>>> func)
+        {
+            var response = await func.Invoke();
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        protected async Task<IActionResult> WithResponseAsync(Func<Task<Response>> func)
+        {
+            var response = await func.Invoke();
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
